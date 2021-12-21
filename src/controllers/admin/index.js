433debc3421b8d0261d803252ef2ec41776bbe02
interrupter <alex.repin@mail.ru>
2021-12-21 23:34:08 +0300

    fix: controllers

--- src/controllers/admin/index.js
+++ src/controllers/admin/index.js
@@ -1,14 +1,14 @@
 import {
-  manifest,
-  wsc,
-  ncNotification,
-  services
+	manifest,
+	wsc,
+	ncNotification,
+	services
 } from '../lib/user.js';
 
 
 export {
-  manifest,
-  wsc,
-  ncNotification,
-  services
+	manifest,
+	wsc,
+	ncNotification,
+	services
 };
