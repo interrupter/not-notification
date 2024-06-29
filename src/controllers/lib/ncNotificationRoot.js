bd343b034f4fd8eec30167f05263f5a214d32a0b
Aleksandr Repin <admin@appmon.ru>
2024-06-29 18:29:07 +0300

    fix: actions without delays for Controllers

--- src/controllers/lib/ncNotificationRoot.js
+++ src/controllers/lib/ncNotificationRoot.js
@@ -1,124 +1,137 @@
-import {
-	Frame,
-	Elements
-} from 'not-bulma';
+import { Frame, Elements } from "not-bulma";
 
 const notCRUD = Frame.notCRUD;
 const UICommon = Elements.UICommon;
 
-const MODULE_NAME = '';
-const MODEL_NAME = 'notification';
+const MODULE_NAME = "";
+const MODEL_NAME = "notification";
 
 const LABELS = {
-	plural: 'not-notification:labelPlural',
-	single: 'not-notification:labelSingle',
+    plural: "not-notification:labelPlural",
+    single: "not-notification:labelSingle",
 };
 
-import NotificationActionInbox from './actions/inbox.js';
-import NotificationActionDetails from './actions/details.js';
+import NotificationActionInbox from "./actions/inbox.js";
+import NotificationActionDetails from "./actions/details.js";
 
 const ACTIONS = {
-  inbox: NotificationActionInbox,
-  details: NotificationActionDetails
+    inbox: NotificationActionInbox,
+    details: NotificationActionDetails,
 };
 
-import NotificationRouter from './router.js';
+import NotificationRouter from "./router.js";
 
 class ncNotification extends notCRUD {
+    static MODULE_NAME = MODULE_NAME;
+    static MODEL_NAME = MODEL_NAME;
 
-	static MODULE_NAME = MODULE_NAME;
-	static MODEL_NAME = MODEL_NAME;
+    constructor(app, params) {
+        super(app, `${MODULE_NAME}.${MODEL_NAME}`, {
+            actions: ACTIONS,
+            router: NotificationRouter,
+        });
+        this.setModuleName(MODULE_NAME.toLowerCase());
+        this.setModelName(MODEL_NAME.toLowerCase());
+        this.setOptions("names", LABELS);
+        this.setOptions("Validators", {});
+        this.setOptions("params", params);
+        this.setOptions("list", {
+            interface: {
+                combined: true,
+                factory: this.getInterface(),
+            },
+            endless: false,
+            preload: {},
+            sorter: {
+                createdAt: -1,
+            },
+            actions: [],
+            showSearch: true,
+            idField: "_id",
+            fields: [
+                {
+                    path: ":title",
+                    title: "not-notification:fieldTitle",
+                    searchable: true,
+                    sortable: true,
+                    maxLength: 100,
+                },
+                {
+                    path: ":text",
+                    title: "not-notification:fieldText",
+                    searchable: true,
+                    sortable: true,
+                    maxLength: 100,
+                },
+                {
+                    path: ":owner",
+                    title: "not-notification:fieldOwner",
+                    searchable: true,
+                    sortable: true,
+                },
+                {
+                    path: ":createdAt",
+                    title: "not-notification:fieldCreatedAt",
+                    sortable: true,
+                    searchable: true,
+                    preprocessor: (value) => {
+                        return UICommon.formatTimestamp(
+                            new Date(value).getTime()
+                        );
+                    },
+                },
+                {
+                    path: ":new",
+                    title: "not-notification:fieldNew",
+                    type: "boolean",
+                    sortable: true,
+                    searchable: true,
+                    preprocessor(value) {
+                        return [{ value }];
+                    },
+                },
+                {
+                    path: ":_id",
+                    title: "not-notification:fieldAction",
+                    type: "button",
+                    preprocessor: (value) => {
+                        return [
+                            {
+                                action: () => this.goDetails(value),
+                                title: "not-notification:actionDetails",
+                                size: "small",
+                            },
+                            {
+                                action: () => this.goDelete(value),
+                                color: "danger",
+                                title: "not-notification:actionDelete",
+                                size: "small",
+                                style: "outlined",
+                            },
+                        ];
+                    },
+                },
+            ],
+        });
+        this.start();
+        return this;
+    }
 
-	constructor(app, params) {
-		super(app, `${MODULE_NAME}.${MODEL_NAME}`, {actions: ACTIONS, router: NotificationRouter});
-		this.setModuleName(MODULE_NAME.toLowerCase());
-		this.setModelName(MODEL_NAME.toLowerCase());
-		this.setOptions('names', LABELS);
-		this.setOptions('Validators', {});
-		this.setOptions('params', params);
-		this.setOptions('list', {
-			interface: {
-				combined: 			true,
-				factory: 				this.getInterface()
-			},
-			endless: false,
-			preload: {},
-			sorter: {
-				createdAt: -1
-			},
-			actions: [],
-			showSearch: true,
-			idField: '_id',
-			fields: [{
-				path: ':title',
-				title: 'not-notification:fieldTitle',
-				searchable: true,
-				sortable: true,
-				maxLength: 100,
-			}, {
-				path: ':text',
-				title: 'not-notification:fieldText',
-				searchable: true,
-				sortable: true,
-				maxLength: 100,
-			}, {
-				path: ':owner',
-				title: 'not-notification:fieldOwner',
-				searchable: true,
-				sortable: true
-			}, {
-				path: ':createdAt',
-				title: 'not-notification:fieldCreatedAt',
-				sortable: true,
-				searchable: true,
-				preprocessor: (value)=>{
-					return UICommon.formatTimestamp(new Date(value).getTime());
-				}
-			},{
-				path: ':new',
-				title: 'not-notification:fieldNew',
-				type: 'boolean',
-				sortable: true,
-				searchable: true,
-				preprocessor(value){return [{value}];}
-			}, {
-				path: ':_id',
-				title: 'not-notification:fieldAction',
-				type: 'button',
-				preprocessor: (value) => {
-					return [
-						{
-							action: this.goDetails.bind(this, value),
-							title: 'not-notification:actionDetails',
-							size: 'small'
-						},
-						{
-							action: this.goDelete.bind(this, value),
-							color: 'danger',
-							title: 'not-notification:actionDelete',
-							size: 'small',
-							style: 'outlined'
-						}
-					];
-				},
-			}]
-		});
-		this.start();
-		return this;
-	}
+    goInbox() {
+        this.app
+            .getWorking("router")
+            .navigate(this.getModelActionURL(false, "inbox"));
+    }
 
-	goInbox() {
-		this.app.getWorking('router').navigate(this.getModelActionURL(false, 'inbox'));
-	}
+    goMarkAsRead(value) {
+        this.app
+            .getWorking("router")
+            .navigate(this.getModelActionURL(value, "markAsRead"));
+    }
 
-	goMarkAsRead(value) {
-		this.app.getWorking('router').navigate(this.getModelActionURL(value, 'markAsRead'));
-	}
-
-	updateNotifications(){
-		this.app.getService('nsNotification').update();
-	}
-
+    updateNotifications() {
+        this.app.getService("nsNotification").update();
+    }
 }
 
 export default ncNotification;
--- src/controllers/lib/ncNotificationUser.js
+++ src/controllers/lib/ncNotificationUser.js
@@ -1,121 +1,130 @@
-import {
-	Frame,
-	Elements
-} from 'not-bulma';
+import { Frame, Elements } from "not-bulma";
 
 const notCRUD = Frame.notCRUD;
 const UICommon = Elements.UICommon;
 
-const MODULE_NAME = '';
-const MODEL_NAME = 'notification';
+const MODULE_NAME = "";
+const MODEL_NAME = "notification";
 
 const LABELS = {
-	plural: 'not-notification:labelPlural',
-	single: 'not-notification:labelSingle',
+    plural: "not-notification:labelPlural",
+    single: "not-notification:labelSingle",
 };
 
-import NotificationActionInbox from './actions/inbox.js';
-import NotificationActionDetails from './actions/details.js';
+import NotificationActionInbox from "./actions/inbox.js";
+import NotificationActionDetails from "./actions/details.js";
 
 const ACTIONS = {
-  list: NotificationActionInbox,
-	inbox: NotificationActionInbox,
-  details: NotificationActionDetails
+    list: NotificationActionInbox,
+    inbox: NotificationActionInbox,
+    details: NotificationActionDetails,
 };
 
-import NotificationRouter from './router.js';
+import NotificationRouter from "./router.js";
 
 class ncNotification extends notCRUD {
+    static MODULE_NAME = MODULE_NAME;
+    static MODEL_NAME = MODEL_NAME;
 
-	static MODULE_NAME = MODULE_NAME;
-	static MODEL_NAME = MODEL_NAME;
+    constructor(app, params, { actions: ACTIONS, router: NotificationRouter }) {
+        super(app, `${MODULE_NAME}.${MODEL_NAME}`);
+        this.setModuleName(MODULE_NAME.toLowerCase());
+        this.setModelName(MODEL_NAME.toLowerCase());
+        this.setOptions("names", LABELS);
+        this.setOptions("Validators", {});
+        this.setOptions("params", params);
+        this.setOptions("list", {
+            interface: {
+                combined: true,
+                combinedAction: "inbox",
+                factory: this.getInterface(this.getModelName()),
+            },
+            endless: false,
+            preload: {},
+            sorter: {
+                createdAt: -1,
+            },
+            actions: [],
+            showSearch: true,
+            idField: "_id",
+            fields: [
+                {
+                    path: ":title",
+                    title: "not-notification:fieldTitle",
+                    searchable: true,
+                    maxLength: 50,
+                    sortable: true,
+                },
+                {
+                    path: ":text",
+                    title: "not-notification:fieldText",
+                    searchable: true,
+                    sortable: true,
+                    maxLength: 50,
+                },
+                {
+                    path: ":createdAt",
+                    title: "not-notification:fieldCreatedAt",
+                    sortable: true,
+                    searchable: true,
+                    preprocessor: (value) => {
+                        return UICommon.formatTimestamp(
+                            new Date(value).getTime()
+                        );
+                    },
+                },
+                {
+                    path: ":new",
+                    title: "not-notification:fieldNew",
+                    type: "boolean",
+                    sortable: true,
+                    searchable: true,
+                    preprocessor(value) {
+                        return [{ value }];
+                    },
+                },
+                {
+                    path: ":_id",
+                    title: "not-notification:fieldAction",
+                    type: "button",
+                    preprocessor: (value) => {
+                        return [
+                            {
+                                action: () => this.goDetails(value),
+                                title: "not-notification:actionDetails",
+                                size: "small",
+                            },
+                            {
+                                action: () => this.goDelete(value),
+                                color: "danger",
+                                title: "not-notification:actionDelete",
+                                size: "small",
+                                style: "outlined",
+                            },
+                        ];
+                    },
+                },
+            ],
+        });
+        this.start();
+        return this;
+    }
 
-	constructor(app, params, { actions: ACTIONS, router: NotificationRouter}) {
-		super(app, `${MODULE_NAME}.${MODEL_NAME}`);
-		this.setModuleName(MODULE_NAME.toLowerCase());
-		this.setModelName(MODEL_NAME.toLowerCase());
-		this.setOptions('names', LABELS);
-		this.setOptions('Validators', {});
-		this.setOptions('params', params);
-		this.setOptions('list', {
-			interface: {
-				combined: 			true,
-				combinedAction: 'inbox',
-				factory: 				this.getInterface(this.getModelName())
-			},
-			endless: false,
-			preload: {},
-			sorter: {
-				createdAt: -1
-			},
-			actions: [],
-			showSearch: true,
-			idField: '_id',
-			fields: [{
-				path: ':title',
-				title: 'not-notification:fieldTitle',
-				searchable: true,
-				maxLength: 50,
-				sortable: true
-			}, {
-				path: ':text',
-				title: 'not-notification:fieldText',
-				searchable: true,
-				sortable: true,
-				maxLength: 50
-			}, {
-				path: ':createdAt',
-				title: 'not-notification:fieldCreatedAt',
-				sortable: true,
-				searchable: true,
-				preprocessor: (value)=>{
-					return UICommon.formatTimestamp(new Date(value).getTime());
-				}
-			},{
-				path: ':new',
-				title: 'not-notification:fieldNew',
-				type: 'boolean',
-				sortable: true,
-				searchable: true,
-				preprocessor(value){return [{value}];}
-			}, {
-				path: ':_id',
-				title: 'not-notification:fieldAction',
-				type: 'button',
-				preprocessor: (value) => {
-					return [
-						{
-							action: this.goDetails.bind(this, value),
-							title: 'not-notification:actionDetails',
-							size: 'small'
-						},
-						{
-							action: this.goDelete.bind(this, value),
-							color: 'danger',
-							title: 'not-notification:actionDelete',
-							size: 'small',
-							style: 'outlined'
-						}
-					];
-				},
-			}]
-		});
-		this.start();
-		return this;
-	}
+    goInbox() {
+        this.app
+            .getWorking("router")
+            .navigate(this.getModelActionURL(false, "inbox"));
+    }
 
-	goInbox() {
-		this.app.getWorking('router').navigate(this.getModelActionURL(false, 'inbox'));
-	}
+    goMarkAsRead(value) {
+        this.app
+            .getWorking("router")
+            .navigate(this.getModelActionURL(value, "markAsRead"));
+    }
 
-	goMarkAsRead(value) {
-		this.app.getWorking('router').navigate(this.getModelActionURL(value, 'markAsRead'));
-	}
-
-	updateNotifications(){
-		this.app.getService('nsNotification').update();
-	}
-
+    updateNotifications() {
+        this.app.getService("nsNotification").update();
+    }
 }
 
 export default ncNotification;
